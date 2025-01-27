54ab9a001cafef78ac672947b1fd9cdf1d898c88
Jayce Bordelon <b.jayce@wustl.edu>
2025-01-27 15:18:55 -0600

    Skely

--- poefree-client/src/App.tsx
+++ poefree-client/src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Auth from './pages/Auth';
+import NotFound from './pages/NotFound';
+
 function App() {
-  return <h1>hello</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
