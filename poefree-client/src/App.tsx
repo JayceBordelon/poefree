c3c66fdc0dc3b52fbe7429d5ffc455745aa77393
Jayce Bordelon <b.jayce@wustl.edu>
2025-01-27 09:13:24 -0600

    git synch

--- poefree-client/src/App.tsx
+++ poefree-client/src/App.tsx
@@ -1,32 +1,7 @@
-import { ServerResponse } from "@backend/responseTypes";
 import "./App.css";
-import { getAllUsers, registerNewUser } from "./api/user";
-import { useState } from "react";
-import { User } from "@backend/prismaTypes";
 
 function App() {
-  const [users, setUsers] = useState<User[]>([]);
-  return (
-    <>
-      <button
-        onClick={async () => {
-          const res: ServerResponse = await registerNewUser({
-            name: "jayce",
-            email: "bad@jayce.com",
-            password: "poop",
-          });
-          const usersResponse: ServerResponse = await getAllUsers();
-          if (usersResponse.payload) {
-            const userList: User[] = usersResponse.payload as User[];
-            setUsers(userList);
-          }
-        }}
-      >
-        Register Fake User
-      </button>
-      {users.length > 0 && users.map((user) => <p>{JSON.stringify(user)}</p>)}
-    </>
-  );
+  return <h1>hello</h1>;
 }
 
 export default App;
